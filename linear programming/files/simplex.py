6329544615da02609d0abac0d5f900bfdf27f44b
junyyy <67784390+junyyy@users.noreply.github.com>
2020-09-27 23:58:49 +1000

    Add files via upload

--- linear programming/files/simplex.py
+++ linear programming/files/simplex.py
@@ -1,9 +1,8 @@
 import argparse
-import os
 import json
-import numpy as np
+import os
 from enum import Enum
-import copy as cp
+import numpy as np
 
 
 class Status(int, Enum):
@@ -50,14 +49,19 @@ class LinearProgram:
         return cls(a, b, c)
 
 
-def row_operation(matrix):
-    matrix[0:] += np.sum(matrix[1:,], axis=0)
+def row_operation(matrix, basis):
+    for i in range(len(basis)):
+        row = i + 1
+        col = basis[i]
+        row_operation_once(matrix, row, col)
 
 
 def row_operation_once(matrix, row, col):
+    # Use in second time to run simplex
+    # row indicates the exiting var
+    # col indicates the entering var
     m, n = matrix.shape
     matrix[row] /= matrix[row, col]
-
     for other_row in range(m):
         if other_row != row:
             matrix[other_row] = matrix[other_row] - matrix[other_row, col] * matrix[row]
@@ -68,53 +72,53 @@ def simplex_inner(lp, basis, tol):
     m = len(lp.b)
     n = len(lp.c)
     # Can only handle problems where (otherwise likely overconstrained)
-    assert(n >= m)
+    assert (n >= m)
     # Need same number of basis variables as constraints
-    assert(len(basis) == m)
+    assert (len(basis) == m)
 
-    # UNIMPLEMENTED
-    c = np.concatenate((-lp.c, [0])).reshape(1, n+1)
+    # concatenate the vectors
+    c = np.concatenate((-lp.c, [0])).reshape(1, n + 1)
     Ab = np.concatenate((lp.a, lp.b.reshape(m, 1)), axis=1)
 
     # In this matrix, the first col is NOT [1, 0, 0, ...] that relates to z
     # so it's shape different is a bit different from what appears in slides
     matrix = np.concatenate((c, Ab))
-
-
-
     matrix_m, matrix_n = matrix.shape
 
+    row_operation(matrix, basis)
 
-    row_operation(matrix)
-    # find the entering variable
     pivots = 0
-
-    while max(matrix[0, 0: matrix_n-1] > tol):
+    while max(matrix[0, 0: matrix_n - 1] > tol):
         # pick one which has the greatest positive coefficient
-        col = np.argmax(matrix[0, 0: matrix_n-1])
-        if max(matrix[1:,col]) <= 0:
+        col = np.argmax(matrix[0, 0: matrix_n - 1])
+        if max(matrix[1:, col]) <= 0:
             # which means that in all rows, all entries are less than or equal to 0
             return {"status": Status.UNBOUNDED, "pivots": pivots}
         row = None
         for i in range(1, matrix_m):
-            if matrix[i, col] > tol and matrix[i, matrix_n - 1] > tol:
+            if matrix[i, col] > tol:
                 if not row:
                     row = i
-                elif matrix[i, matrix_n-1]/matrix[i, col] < matrix[row, matrix_n-1]/matrix[row, col]:
+                elif matrix[i, matrix_n - 1] / matrix[i, col] < matrix[row, matrix_n - 1] / matrix[row, col]:
                     row = i
-        if not row:
-            print(matrix[:, col])
-            print(matrix[:, matrix_n - 1])
-        basis[row-1] = col
+
+        basis[row - 1] = col
         row_operation_once(matrix, row, col)
+
         pivots += 1
 
-    opt_val = matrix[0, matrix_n-1]
+    opt_val = matrix[0, matrix_n - 1]
     x = np.zeros(matrix_n)
     for i in range(1, matrix_m):
-        for j in range(matrix_n-1):
+        for j in range(matrix_n - 1):
             if matrix[i, j] == 1:
-                x[j] = matrix[i, matrix_n-1]
+                x[j] = matrix[i, matrix_n - 1]
+
+    lp.a = matrix[1:,0: n-m]
+    print("jjjjjjj")
+    print(lp.a)
+    lp.b = matrix[1:, matrix_n-1]
+    print(lp.b)
     return {"status": Status.OPTIMAL, "pivots": pivots, "x": x, "basis": basis, "objective": opt_val}
 
 
@@ -138,39 +142,40 @@ def simplex(lp, tol=1e-9):
     # Solve a modified version of the problem to get a starting BFS
     # UNIMPLEMENTED
 
+
     m = len(lp.b)
     n = len(lp.c)
     s = np.identity(m)
-    # copy the original a and c
-    a = lp.a
-    c = lp.c
 
     # set the new a and c to run the simplex
     # in order to get the starting bfs
+    c = lp.c
+
 
     lp.a = np.concatenate((lp.a, s), axis=1)
     lp.c = np.concatenate((np.zeros(n), np.ones(m)))
 
-    slack_basis = [i for i in range(n, n+m)]
+    slack_basis = [i for i in range(n, n + m)]
     res = simplex_inner(lp, slack_basis, tol)
 
     # if any slack variable is non-zero, the original problem is infeasible.
     x = res["x"]
-    print("++++++++++++++++++")
-    print(x[n:m+n])
-    # get the new BFS
-    basis = res["basis"]
     # get the bfs_pivots
     bfs_pivots = res["pivots"]
+    # get the basis
+    basis = res["basis"]
     # get the objective
     objective = res["objective"]
     if abs(objective) > tol:
         return {"status": Status.INFEASIBLE, "bfs_pivots": bfs_pivots}
 
     # if the problem is feasible, then run the simplex again
-    # roll back the original matrix
-    lp.a = a
+    # change c back to original one
     lp.c = c
+    print("SHapesssssssssssss")
+    print(lp.a.shape)
+    print(lp.b.shape)
+    print(lp.c.shape)
     new_res = simplex_inner(lp, basis, tol)
     opt_pivots = new_res["pivots"]
     if new_res["status"] == Status.UNBOUNDED:
@@ -186,17 +191,6 @@ def simplex(lp, tol=1e-9):
             "basis": basis,
             "objective": objective
             }
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
